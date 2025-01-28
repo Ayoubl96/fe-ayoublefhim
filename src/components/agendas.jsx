cf0485b675f779e0e4377328c1487d0f97a330e6
Ayoub Lefhim <37153245+Ayoubl96@users.noreply.github.com>
2025-01-28 05:59:26 +0100

    Add sorting on last agendas posts

--- src/components/agendas.jsx
+++ src/components/agendas.jsx
@@ -9,30 +9,35 @@ const Agenda = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message || "Something went wrong"}</p>;
 
-
   if (!data || !data.data || !Array.isArray(data.data)) {
     return <p>No experiences found.</p>;
   }
 
-  const agendas = data.data;
+  // Sort the agendas array in descending order based on the `createdAt` property
+  const agendas = data.data.sort((a, b) => {
+    const dateA = new Date(a.attributes.createdAt);
+    const dateB = new Date(b.attributes.createdAt);
+    return dateB - dateA; // Descending order
+  });
+
   return (
     <section className="lastposts py-4">
-      {agendas.slice(0, 5).map((agendas) => {
-        const formattedDate = formatDate(agendas.createdAt); // Correct placement of variable
+      {agendas.slice(0, 5).map((agenda) => {
+        const formattedDate = formatDate(agenda.attributes.createdAt);
 
         return (
           <div
-            key={agendas.week}
+            key={agenda.id}
             className="w-full grid py-2 lg:grid-cols-12 border-b border-slate-500"
           >
             <div className="lg:order-2 lg:col-span-3 lg:justify-self-end  dark:text-white">
               {formattedDate}
             </div>
             <Link
-              to={"/agenda/" + agendas.id}
+              to={"/agenda/" + agenda.id}
               className="lg:order-1 lg:col-span-9 font-semibold text-black transition hover:text-primary hover:translate-x-1  dark:text-white"
             >
-              {agendas.week}
+              {agenda.attributes.week}
             </Link>
           </div>
         );
