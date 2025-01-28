6ee6e7cd00f53d3c9e841fce8a5d0c2aead3c62a
Ayoub Lefhim <37153245+Ayoubl96@users.noreply.github.com>
2025-01-28 06:16:32 +0100

    update sorting for agenda page

--- src/components/agenda_loop.jsx
+++ src/components/agenda_loop.jsx
@@ -4,35 +4,42 @@ import useFetch from "../hooks/useFetch";
 
 const Agenda_loops = () => {
   const apiUrl = `${import.meta.env.VITE_API_URL}api/agendas?populate=*`;
-  let { loading, data, error } = useFetch(apiUrl);
+  const { loading, data, error } = useFetch(apiUrl);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message || "Something went wrong"}</p>;
 
-
+  // Controllo che `data.data` sia un array
   if (!data || !data.data || !Array.isArray(data.data)) {
+    console.error("Unexpected data structure:", data);
     return <p>No experiences found.</p>;
   }
 
-  const agendas = data.data;
+  // Ordina i dati per `createdAt` in ordine decrescente
+  const agendas = [...data.data].sort((a, b) => {
+    const dateA = new Date(a.createdAt);
+    const dateB = new Date(b.createdAt);
+    return dateB - dateA; // Ordina in ordine decrescente
+  });
+
   return (
     <>
-      {agendas.map((agendas) => {
+      {agendas.map((agenda) => {
         // Format the date before rendering it
-        const formattedDate = formatDate(agendas.createdAt);
+        const formattedDate = formatDate(agenda.createdAt);
         return (
           <div
-            key={agendas.id} // Use post.id as the key for better uniqueness
+            key={agenda.id} // Use agenda.id as the key for better uniqueness
             className="w-full grid py-2 lg:grid-cols-12 border-b"
           >
-            <div className="lg:col-span-2 lg:order-3 lg:place-self-end  dark:text-white">
+            <div className="lg:col-span-2 lg:order-3 lg:place-self-end dark:text-white">
               {formattedDate}
             </div>
             <Link
-              to={`/agenda/${agendas.id}`}
-              className="lg:col-span-8 font-semibold text-black lg:order-1 transition hover:text-primary hover:translate-x-1  dark:text-white"
+              to={`/agenda/${agenda.id}`}
+              className="lg:col-span-8 font-semibold text-black lg:order-1 transition hover:text-primary hover:translate-x-1 dark:text-white"
             >
-              {agendas.week}
+              {agenda.week}
             </Link>
           </div>
         );
